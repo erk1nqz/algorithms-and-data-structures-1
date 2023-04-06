3a6231984d62482a5bfc2aec6a3164bad40b57c7
erk1nqz <221326@astanait.edu.kz>
2023-04-06 19:53:56 +0600

    sixth  problem

--- src/sixth.java
+++ src/sixth.java
@@ -7,6 +7,7 @@ public class sixth {
         int n = sc.nextInt();
         System.out.println(getPower(a, n));
     }
+
     public static int getPower(int a, int n) {
         if (n == 1) {
             return a;
