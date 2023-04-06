b14d1658d71ce50784891c4f989562b328eff53f
erk1nqz <221326@astanait.edu.kz>
2023-04-06 19:53:08 +0600

    fifth  problem

---
+++ src/sixth.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class sixth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(getPower(a, n));
+    }
+    public static int getPower(int a, int n) {
+        if (n == 1) {
+            return a;
+        }
+        else {
+            return a * getPower(a, n-1);
+        }
+    }
+}
