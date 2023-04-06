eb7080ed2e6953bd9c3d9d7b9d0c4fa1ce9ebb8e
erk1nqz <221326@astanait.edu.kz>
2023-04-06 19:24:13 +0600

    third problem

---
+++ src/third.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class third {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(isPrime(n));
+    }
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
