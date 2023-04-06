ffbcc5285009cb13cd3187bcad1dafebfa3d0d69
erk1nqz <221326@astanait.edu.kz>
2023-04-06 18:56:18 +0600

    first problem

---
+++ src/second.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class second {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(findAverage(n, arr));
+    }
+    public static double findAverage(int n, int[] arr) {
+        double sum = arr[0];
+        for (int i = 1; i < n; i++) {
+            sum += arr[i];
+        }
+        return sum / n;
+    }
+}
