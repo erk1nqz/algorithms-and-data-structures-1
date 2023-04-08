04701fd721a1ea6cd6cd8faeff9c866a682d9143
erk1nqz <221326@astanait.edu.kz>
2023-04-08 17:15:54 +0600

    tenth problem

---
+++ src/tenth.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class tenth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(findGCD(a, b));
+    }
+    public static int findGCD(int a, int b) {
+        if (b == 0) return a;
+        else return findGCD(b, a % b);
+    }
+}
